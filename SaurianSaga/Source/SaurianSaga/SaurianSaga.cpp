30660a343bcedc9bd3e7d4b4b8960aa88aa61b08
Jack Hannan <jackhannan2k353@gmail.com>
2026-06-04 16:55:35 -0400

    Remove UE5 template

--- SaurianSaga/Source/SaurianSaga/SaurianSaga.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SaurianSaga.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SaurianSaga, "SaurianSaga" );
-
-DEFINE_LOG_CATEGORY(LogSaurianSaga)
--- SaurianSaga/Source/SaurianSaga/SaurianSagaGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SaurianSagaGameMode.h"
-
-ASaurianSagaGameMode::ASaurianSagaGameMode()
-{
-	// stub
-}
