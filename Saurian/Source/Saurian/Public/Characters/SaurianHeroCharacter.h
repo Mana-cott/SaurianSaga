4166bd58b4f5fe17ae71a51cc85b21ed1ed1fb29
Jack Hannan <jackhannan2k353@gmail.com>
2026-07-25 22:34:09 -0400

    Setup base classes

---
+++ Saurian/Source/Saurian/Public/Characters/SaurianHeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/SaurianBaseCharacter.h"
+#include "SaurianHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAURIAN_API ASaurianHeroCharacter : public ASaurianBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
