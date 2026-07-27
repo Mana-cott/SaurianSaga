469348b9d6db1a5935fbc36d733cad5703372997
Jack Hannan <jackhannan2k353@gmail.com>
2026-07-27 14:26:54 -0400

    Setup Gameplay Tags

---
+++ Saurian/Source/Saurian/Public/SaurianGameplayTags.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace SaurianGameplayTags
+{
+	/** Input Tags **/
+	SAURIAN_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	SAURIAN_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
